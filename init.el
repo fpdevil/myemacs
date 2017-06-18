eb54df02b766f825dd48e7211499ad1aded46dd9
fpdevil <singamsetty.sampath@gmail.com>
2017-06-18 13:21:10 +0530

    updated to load individual configurations, also load-paths moved from aqua-package-repos to here

--- init.el
+++ init.el
@@ -18,8 +18,8 @@
 ;;;
 ;;; Code:
 ;;        This sets up the load path so that we can override it
-;;        Update Note    : 11 March 2017
-;;;===========================================================================
+;;        Update Note    : 18 June 2017
+;;;=============================================================================
 
 ;; utf-8 character set encoding and Locale
 (set-language-environment   'utf-8)
@@ -40,122 +40,116 @@
          version-separator
          emacs-minor-version)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; adding location for required lisp files and libraries to the path      ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq user-emacs-directory (concat (getenv "HOME") "/.emacs.d"))
-(add-to-list 'load-path (concat (getenv "HOME") "/.emacs.d"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; load the newest byte code every time                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq load-prefer-newer t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; adding location for required lisp files and libraries to the path        ;;;
+;; (setq user-emacs-directory (concat (getenv "HOME") "/.emacs.d"))          ;;;
+;; (add-to-list 'load-path (concat (getenv "HOME") "/.emacs.d"))             ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq user-emacs-directory  "~/.emacs.d")
+(add-to-list 'load-path user-emacs-directory)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; custom-settings.el will store any custom settings made on Emacs        ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   custom-settings.el will store any custom settings made on Emacs        ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq custom-file
       (expand-file-name "custom-settings.el" user-emacs-directory))
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;;----------------------------------------------------------------------------;;
+;;; Define custom directories for the packages                               ;;;
+;;; packages/elpa will contain the standard packages installed by Emacs      ;;;
+;;; modules dir will contain the custom built and lang specific modules      ;;;
+;;; vendor dir will contain 3rd party or unavailable packages                ;;;
+;;; Define a top-level, vendor and custom files                              ;;;
+;;----------------------------------------------------------------------------;;
+(defconst home-dir "~")
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; load custom methods and internal settings for Emacs                    ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load (concat user-emacs-directory "/core/aqua-methods.el"))
-;; now load the custom settings from aqua-internals.el
-(add-hook 'after-init-hook
-          '(lambda ()
-             (load (concat user-emacs-directory "/core/aqua-internals.el"))))
+(defvar emacs-dir (file-name-directory load-file-name)
+  "Top level Emacs dir.")
+(defvar emacs-dir (file-name-directory (concat (getenv "HOME") "/.emacs.d"))
+  "Top level Emacs dir.")
+(defvar core-dir (expand-file-name "core" emacs-dir)
+  "Contains core components like the package repository information etc.")
+(defvar vendor-dir (expand-file-name "vendor" emacs-dir)
+  "Packages not yet available in ELPA.")
+(defvar module-dir (expand-file-name "modules" emacs-dir)
+  "Personal stuff.")
+(defvar personal-dir (expand-file-name "personal" emacs-dir)
+  "All personal configuration settings like theme names etc.")
+(defvar save-dir (expand-file-name "cache" emacs-dir)
+  "Common directory for automatically generated save/history/files/etc.")
+(defvar pkg-dir (expand-file-name "packages" emacs-dir)
+  "Package installation directory for all Emacs packages.")
+
+;;----------------------------------------------------------------------------;;
+;;;;;;;;;;          end of custom directory declaration.              ;;;;;;;;;;
+;;----------------------------------------------------------------------------;;
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; aqua-package-repos.el will load the package repository settings        ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; (load (concat (getenv "HOME") "/.emacs.d/aqua-package-repos.el"))
-(load (concat user-emacs-directory "/aqua-package-repos.el"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   load core package, custom methods and internal settings for Aquamacs   ;;;
+;;;   aqua-packages.el
+;;;   aqua-methods.el
+;;;   aqua-internals.el
+;;;   aqua-ui.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-to-list 'load-path core-dir)             ;; load the core dir first
+(message "Loading the aquamac's core...")
+
+(require 'aqua-packages)
+(require 'aqua-methods)
+(require 'aqua-internals)
+(require 'aqua-ui)
+(require 'aqua-customizations)
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; byte recompiling everything during bootstrap (comment | uncomment)     ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   aqua-package-repos.el will load the package repository settings        ;;;
+;;;   (load (concat (getenv "HOME") "/.emacs.d/aqua-package-repos.el"))      ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq aqua-pkg-repos
+      (expand-file-name "aqua-package-repos.el" user-emacs-directory))
+(when (file-exists-p aqua-pkg-repos)
+  (message "Loading the aquqmacs package installer...")
+  (load aqua-pkg-repos))
+
+;; (load (concat user-emacs-directory "/aqua-package-repos.el"))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   byte recompiling everything during bootstrap (comment | un-comment)    ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun byte-recompile-init-files ()
   "Recompile all of the startup files."
   (interactive)
-  (byte-recompile-directory (concat (getenv "HOME") "/.emacs.d/packages") 0))
-(setq load-prefer-newer t)             ;; load the newest byte code every time
-(setq debug-on-error t)                ;; finalizers (for debugging)
-
-(byte-recompile-init-files)            ;; comment or uncomment
-
-;;============================================================================
-;;;  specify all the require packages and settings to be loaded initially
-;;============================================================================
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Diminished modes are minor modes with no modeline display              ;;;
-;;; hide a minor mode that you know are always enabled using this          ;;;
-;;; http://www.eskimo.com/~seldon/diminish.el                              ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'diminish)
-(eval-after-load "whitespace" '(diminish 'whitespace-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; identify unnecessary whitespace is in all programming modes            ;;;
-;;; whitespace-cleanup command for clearing trailing white spaces          ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'whitespace)
-(setq-default show-trailing-whitespace t)
-(set-default 'indent-tabs-mode nil)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; magit (git integration - now using git-gutter)                         ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; (require 'magit)
-; (eval-after-load 'magit
-;   (progn '(global-set-key (kbd "C-x g") 'magit-status)))
-; (define-key global-map (kbd "C-c m") 'magit-status)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; flymake-easy (helpers for easily building Emacs flymake checkers)      ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'flymake-easy)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; show flymake errors in mini buffer                                     ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'flymake-cursor)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; buffer mode                                                            ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'buffer-move)
-(global-set-key (kbd "<S-s-up>")     'buf-move-up)
-(global-set-key (kbd "<S-s-down>")   'buf-move-down)
-(global-set-key (kbd "<S-s-left>")   'buf-move-left)
-(global-set-key (kbd "<S-s-right>")  'buf-move-right)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Map pairs of simultaneously pressed keys to commands                   ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'key-chord)
-(key-chord-define-global "fm" 'list-buffers)
-(key-chord-define-global "fm" 'helm-mini)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; edit multiple regions simultaneously in a buffer or a region           ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require 'iedit)
-(setq iedit-unmatched-lines-invisible-default t)
+  (byte-recompile-directory (concat user-emacs-directory "/packages") 0))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Make Emacs use the $PATH set up by the user's shell                    ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; finalizers (for debugging and recompiling)                               ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (setq debug-on-error t)
+;; (setq debug-on-signal t)
+(byte-recompile-init-files)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   Make Emacs use the $PATH set up by the user's shell                    ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; set SHELL and pull PATH variables from the .zshrc                      ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   set SHELL and pull PATH variables from the .zshrc                      ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; set shell
 (setenv "SHELL" "/bin/zsh")
 (defun set-exec-path-from-shellpath ()
@@ -180,8 +174,13 @@
   (setenv "PATH" (concat dir ":" (getenv "PATH")))
   (add-to-list 'exec-path dir))
 
+ ;; environment
+  (setq exec-path-from-shell-arguments '("-l"))
+  (setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
+  (setq exec-path (append exec-path '("/usr/local/bin")))
+
 ;;; custom init settings completed
-;;------------------------------------------------------------------------------
+;;--------------------------------------------------------------------------------
 
 (provide 'init)
 ;;; init.el ends here
