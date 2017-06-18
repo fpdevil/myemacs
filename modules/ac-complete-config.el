3dd7c60ea5242e485ba106b6d5b843fc97d54e73
fpdevil <singamsetty.sampath@gmail.com>
2017-06-18 13:30:58 +0530

    minor changes for timeout

--- modules/ac-complete-config.el
+++ modules/ac-complete-config.el
@@ -11,21 +11,26 @@
 ;;;===========================================================================
 (require 'auto-complete)
 (require 'pos-tip)
+(require 'ac-slime)
+
 ;;;
 ;;; Code:
 ;;;
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; auto-completion (where company is not available)                         ;;
 ;; setting up autocomplete after yasnippet to avoid duplicate tab bindings  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (when (require 'auto-complete-config nil 'noerror)
   (add-to-list 'ac-dictionary-directories
-    (concat (getenv "HOME") "/.emacs.d/cache/auto-complete/ac-dict"))
+               (concat user-emacs-directory "/cache/auto-complete/ac-dict"))
   (setq ac-comphist-file
-    (concat (getenv "HOME") "/.emacs.d/cache/auto-complete/ac-comphist.dat"))
-  (ac-config-default)
-  )
+        (concat user-emacs-directory "/cache/auto-complete/ac-comphist.dat"))
+  (ac-config-default))
 
+;;===========================================================================
+;; some default global values for ac completions
+;;===========================================================================
 (setq ac-auto-show-menu t
       ac-delay 0.6
       ac-quick-help-delay 0.8
@@ -43,17 +48,50 @@
 (setq ac-trigger-commands
       (cons 'backward-delete-char-untabify ac-trigger-commands))
 
+;;===========================================================================
 ;; show the menu
+;;===========================================================================
 (setq ac-show-menu-immediately-on-auto-complete t)
 
-(setq-default ac-sources '(ac-source-dictionary
-                           ac-source-words-in-buffer
-                           ac-source-words-in-all-buffer
-                           ac-source-functions
-                           ac-source-yasnippet
-                           ac-source-abbrev
-                           ac-source-words-in-same-mode-buffers))
+;;===========================================================================
+; set the default sources for auto completion
+;;===========================================================================
+;; (setq-default ac-sources '(ac-source-dictionary
+;;                            ac-source-words-in-buffer
+;;                            ac-source-words-in-all-buffer
+;;                            ac-source-functions
+;;                            ac-source-yasnippet
+;;                            ac-source-abbrev
+;;                            ac-source-words-in-same-mode-buffers))
 
+;; define the same using function
+(defvar aqua/ac-sources-default
+  "Default ac completion sources."
+  '(ac-source-dictionary
+    ac-source-words-in-buffer
+    ac-source-words-in-all-buffer
+    ac-source-functions
+    ac-source-yasnippet
+    ac-source-abbrev
+    ac-source-words-in-same-mode-buffers))
+
+(setq-default ac-sources aqua/ac-sources-default)
+
+;;===========================================================================
+; add on for individual modes if any
+;;===========================================================================
+(defun aqua/add-sources-for-prog ()
+  "Add ac-sources for each programming-mode."
+  (let ((add-source
+                 (lambda (s)
+                   (setq ac-sources (append `(,s) aqua/ac-sources-default)))))
+    (cl-case major-mode
+      (go-mode      (funcall add-source 'ac-source-go))
+      (haskell-mode (add-to-list 'ac-sources 'ac-source-ghc-mod)))))
+
+;;===========================================================================
+; add to programming hooks
+;;===========================================================================
 (add-hook 'emacs-lisp-mode-hook 'ac-emacs-lisp-mode-setup)
 (add-hook 'c-mode-common-hook 'ac-cc-mode-setup)
 (add-hook 'css-mode-hook 'ac-css-mode-setup)
@@ -79,6 +117,20 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; add lisp autocomplete-support
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(add-hook 'slime-mode-hook 'set-up-slime-ac)
+(add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes 'slime-repl-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (provide 'ac-complete-config)
+
+;; Local Variables:
+;; coding: utf-8
+;; mode: emacs-lisp
+;; End:
 
 ;;; ac-complete-config.el ends here
