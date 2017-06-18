0710d2e2e249c825ba61021aa17be9b93517eb2a
fpdevil <singamsetty.sampath@gmail.com>
2017-06-18 13:41:32 +0530

    minor update for smart parens config

--- modules/smart-config.el
+++ modules/smart-config.el
@@ -17,12 +17,15 @@
 ;; pairs and tries to be smart about it.                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (smartparens-global-mode t)
-(show-smartparens-global-mode t)
-
 
 (sp-local-pair '(emacs-lisp-mode) "'" "'" :actions nil)
 (sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
+(setq sp-base-key-bindings 'paredit)
+(setq sp-autoskip-closing-pair 'always)
+(setq sp-hybrid-kill-entire-symbol nil)
+(sp-use-paredit-bindings)
 
+(show-smartparens-global-mode +1)
 
 (provide 'smart-config)
 ;;; smart-config.el ends here
