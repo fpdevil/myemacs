ecbe45ceb5287483071b9a4194b901f0b3dfa669
fpdevil <singamsetty.sampath@gmail.com>
2017-09-22 04:02:07 +0530

    updated company backends and ielm mode

--- modules/elisp-config.el
+++ modules/elisp-config.el
@@ -191,62 +191,12 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; an auto insert template for new elisp files
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(eval-after-load 'autoinsert
-  '(define-auto-insert '("\\.el\\'" . "my Emacs Lisp header")
-     '(
-       "Short description: "
-       ";;; " (file-name-nondirectory (buffer-file-name)) " --- " str
-       (make-string (max 2 (- 80 (current-column) 27)) ?\s)
-       "-*- lexical-binding: t; -*-" '(setq lexical-binding t)
-       "\n
-;; Copyright (C) " (format-time-string "%Y") "  "
-       (getenv "ORGANIZATION") | (progn user-full-name) "
-
-;; Author: " (user-full-name)
-       '(if (search-backward "&" (line-beginning-position) t)
-            (replace-match (capitalize (user-login-name)) t t))
-       '(end-of-line 1) " <" (progn user-mail-address) ">
-;; Keywords: "
-       '(require 'finder)
-       ;;'(setq v1 (apply 'vector (mapcar 'car finder-known-keywords)))
-       '(setq v1 (mapcar (lambda (x) (list (symbol-name (car x))))
-                         finder-known-keywords)
-              v2 (mapconcat (lambda (x) (format "%12s:  %s" (car x) (cdr x)))
-                            finder-known-keywords
-                            "\n"))
-       ((let ((minibuffer-help-form v2))
-          (completing-read-ido-ubiquitous "Keyword, C-h: " v1 nil t))
-        str ", ") & -2 "
-
-\;; This program is free software; you can redistribute it and/or modify
-\;; it under the terms of the GNU General Public License as published by
-\;; the Free Software Foundation, either version 3 of the License, or
-\;; (at your option) any later version.
-
-\;; This program is distributed in the hope that it will be useful,
-\;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-\;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-\;; GNU General Public License for more details.
-
-\;; You should have received a copy of the GNU General Public License
-\;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-\;;; Commentary:
-
-\;; " _ "
-
-\;;; Code:
-
-
-\(provide '"
-       (file-name-base)
-       ")
-\;;; " (file-name-nondirectory (buffer-file-name)) " ends here\n")))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'elisp-config)
+
+;; Local Variables:
+;; coding: utf-8
+;; mode: emacs-lisp
+;; End:
 
 ;;; elisp-config.el ends here
