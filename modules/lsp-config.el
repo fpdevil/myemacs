eafafed2a63c456e48fcb988d7a7b1c34981d5b2
fpdevil <singamsetty.sampath@gmail.com>
2020-10-11 12:30:56 -0500

    updated lsp options

--- modules/lsp-config.el
+++ modules/lsp-config.el
@@ -1,101 +1,92 @@
-;;------------------------------------------------------------------------------
-;; EGLOT and LSP - Client for LSP Servers
-;;------------------------------------------------------------------------------
+;;; package --- language server protocol configuration settings
+;;;
+;;; Commentary:
+;;; Filename   : lsp-config.el
+;;;              EGLOT and LSP - Client for LSP Servers
+;;; Code:
+;;;
 
-(use-package lsp-mode
-  :ensure t
-  :defer t
-  :hook
-  (go-mode . lsp-deferred)
-  :commands (lsp lsp-deferred)
-  :custom
-  (lsp-prefer-flymake nil)
-  :config
 
-  ;; change nil to 't to enable logging of packets between emacs and the LS
-  ;; this was invaluable for debugging communication with the MS Python Language Server
-  ;; and comparing this with what vs.code is doing
-  (setq lsp-print-io nil)
+(require 'lsp-mode)
+(require 'lsp-ui)
+(require 'lsp-treemacs)
+(require 'lsp-ivy)
+(require 'company-lsp)
+(require 'dap-mode)
+(require 'dap-ui)
 
-  ;; lsp-ui gives us the blue documentation boxes and the sidebar info
-  (use-package lsp-ui
-    :ensure t
-    :requires lsp-mode flycheck
-    :config
-    (setq lsp-ui-doc-enable t
-          lsp-ui-doc-use-childframe t
-          lsp-ui-doc-position 'top
-          lsp-ui-doc-include-signature t
-          lsp-ui-sideline-enable nil
-          lsp-ui-flycheck-enable t
-          lsp-ui-flycheck-list-position 'right
-          lsp-ui-flycheck-live-reporting t
-          lsp-ui-peek-enable t
-          lsp-ui-peek-list-width 60
-          lsp-ui-peek-peek-height 25
-          lsp-ui-sideline-ignore-duplicate t)
-    (add-hook 'lsp-mode-hook 'lsp-ui-mode))
 
-  ;; install LSP company backend for LSP-driven completion
-  (use-package company-lsp
-    :requires company
-    :config
+(defun /aqua/lsp-init ()
+  "Aqua lsp mode init."
+  (lsp-flycheck-enable)
+  (flycheck-inline-mode -1)
+  (when lsp-enable-symbol-highlighting
+    (add-hook #'lsp-on-idle-hook #'lsp--document-highlight nil t)
+    (lsp--info "Symbol highlighting enabled in current buffer."))
+  (lsp-enable-which-key-integration))
+
+
+(after 'lsp-mode
+  (setq lsp-auto-configure t
+        lsp-prefer-flymake nil
+        lsp-eldoc-render-all t
+        lsp-eldoc-enable-hover t
+        lsp-auto-guess-root t
+        lsp-log-io t
+        lsp-enable-snippet nil
+        lsp-enable-folding nil
+        lsp-enable-snippet nil
+        lsp-enable-completion-at-point nil
+        lsp-enable-links nil
+        lsp-restart 'auto-restart
+        lsp-print-performance t
+        lsp-client-packages '(lsp-clients)
+        lsp-session-file (expand-file-name (locate-user-emacs-file "cache/.lsp-session-v1")))
+
+  ;;Optional - provides fancier overlays.
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode)
+  (setq lsp-ui-doc-enable t
+        lsp-ui-doc-header t
+        lsp-ui-doc-use-childframe t
+        lsp-ui-doc-position 'top
+        lsp-ui-doc-include-signature t
+        lsp-ui-flycheck-list-position 'bottom
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-ignore-duplicate t
+        lsp-ui-sideline-show-symbol t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-code-actions t
+        lsp-ui-peek-enable t
+        lsp-ui-peek-list-width 60
+        lsp-ui-peek-peek-height 25
+        lsp-ui-imenu-enable t
+        lsp-ui-imenu-kind-position 'top)
+  (when (memq window-system '(mac ns))
+    (setq lsp-ui-doc-use-childframe nil))
+)
+
+
+;; Debug Adapter Procol
+(dap-ui-mode 1)
+;; enables mouse hover support
+(dap-tooltip-mode 1)
+(add-hook 'lsp-mode-hook 'dap-mode)
+(add-hook 'lsp-mode-hook 'dap-ui-mode)
+
+;; company mode is a standard completion package that works well with lsp-mode.
+; ;company-lsp integrates company mode completion with lsp-mode.
+;; completion-at-point also works out of the box but doesn't support snippets.
+(after 'company
     (push 'company-lsp company-backends))
 
-  (require 'lsp-clients)
-  ;; (add-hook 'typescript-mode-hook 'lsp)
-  ;; (add-hook 'js2-mode-hook 'lsp)
-  ;; (add-hook 'php-mode 'lsp)
-  ;; (add-hook 'css-mode 'lsp)
-  ;; (add-hook 'ruby-mode 'lsp)
-  ;; (add-hook 'web-mode 'lsp)
-  (setq lsp-prefer-flymake nil)
-  (setq lsp-enable-file-watchers nil)
-  )
-
-;;(use-package company-lsp :ensure t :defer t :hook (lsp-mode))
-
-;; (setq lsp-language-id-configuration '(
-;;                       (java-mode . "java")
-;; 				      ;; (python-mode . "python")
-;; 				      ;; (gfm-view-mode . "markdown")
-;; 				      ;; (rust-mode . "rust")
-;; 				      ;; (ruby-mode . "ruby")
-;; 				      ;; (css-mode . "css")
-;; 				      ;; (xml-mode . "xml")
-;; 				      ;; (c-mode . "c")
-;; 				      ;; (c++-mode . "cpp")
-;; 				      ;; (objc-mode . "objective-c")
-;; 				      ;; (web-mode . "html")
-;; 				      ;; (html-mode . "html")
-;; 				      ;; (sgml-mode . "html")
-;; 				      ;; (mhtml-mode . "html")
-;; 				      ;; (go-mode . "go")
-;; 				      ;; (haskell-mode . "haskell")
-;; 				      ;; (php-mode . "php")
-;; 				      ;; (json-mode . "json")
-;; 				      ;; (js2-mode . "javascript")
-;; 				      ;;(typescript-mode . "typescript")
-;; 				      ))
-
-;; LSP debugging
-(setq lsp-print-io t)
-(setq lsp-trace t)
-(setq lsp-print-performance t)
-
-(use-package dap-mode
-  :after lsp-mode
-  :defer t
-  ;;:hook (lsp-mode . dap-mode)
-  :config
-    (dap-mode t)
-    (dap-ui-mode t))
-
-(use-package eglot
-  :pin melpa-stable
-  :config
-  (add-to-list 'eglot-server-programs '(haskell-mode . ("hie" "--lsp" "--debug")))
-  ;; (optional) Automatically start metals for haskell files.
-  :hook (haskell-mode . eglot-ensure))
 
 (provide 'lsp-config)
+
+;; Local Variables:
+;; coding: utf-8
+;; mode: emacs-lisp
+;; byte-compile-warnings: (not cl-functions)
+;; End:
+
+;;; lsp-config.el ends here
