b13689e9d90b2c48d54710e6c91cb339771054b2
fpdevil <singamsetty.sampath@gmail.com>
2020-10-11 12:33:43 -0500

    included RUST_SRC_PATH

--- modules/exec-path-config.el
+++ modules/exec-path-config.el
@@ -11,24 +11,17 @@
 ;;; Code:
 ;;;
 
-; (after 'exec-path-from-shell
-;   '(progn
-;      (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO"))
-;        (add-to-list 'exec-path-from-shell-variables var)))
-;   (setq exec-path-from-shell-check-startup-files nil)
-;   (when (memq window-system '(mac ns))
-;     (exec-path-from-shell-initialize)))
-
 (use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :ensure t
   :custom
   (shell-file-name "/bin/zsh")
-  (exec-path-from-shell-variables '("PATH" "MANPATH" "PKG_CONFIG_PATH"))
-  :init
-  (if (string-equal system-type "darwin")
-      (exec-path-from-shell-initialize)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;; exec path configuration end ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  (exec-path-from-shell-variables '("PATH"
+                                    "MANPATH"
+                                    "PKG_CONFIG_PATH"
+                                    "RUST_SRC_PATH"))
+  :config
+  (exec-path-from-shell-initialize))
 
 (provide 'exec-path-config)
 
