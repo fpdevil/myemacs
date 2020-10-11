05dfae914cb44b0b702b3079b0c20569ece05d95
fpdevil <singamsetty.sampath@gmail.com>
2020-10-11 12:49:41 -0500

    cached data

---
+++ SessionDesktop.el
@@ -0,0 +1,58 @@
+;; -*- mode: emacs-lisp; coding: emacs-mule; -*-
+;; --------------------------------------------------------------------------
+;; Desktop File for Emacs
+;; --------------------------------------------------------------------------
+;; Created Sun Oct 11 05:31:22 2020
+;; Desktop file format version 208
+;; Emacs version 25.3.50.1
+(setq revive:frame-configuration-to-restore
+  '(progn 
+     (revive:restore-frame '((tool-bar-position . top) (explicit-name) (icon-name) (bottom-divider-width . 0) (right-divider-width . 0) (top . 23) (left . 0) (unsplittable) (modeline . t) (width . 86) (height . 27) (ns-appearance . light) (ns-transparent-titlebar . t) (powerline-cache . #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8 data ((G10 sml/global powerline-active1) (image :type xpm :data "/* XPM */ static char * arrow_left[] = { \"9 19 3 1\", \"0 c #000000\", \"1 c #f5f5f9\", \"2 c #7a7a7c\",\"111111111\",\"011111111\",\"001111111\",\"000111111\",\"000011111\",\"000001111\",\"000000111\",\"000000011\",\"000000001\",\"000000000\",\"000000001\",\"000000011\",\"000000111\",\"000001111\",\"000011111\",\"000111111\",\"001111111\",\"011111111\",\"111111111\",};" :ascent center :face powerline-active1) (G10 powerline-active1 powerline-active2) (image :type xpm :data "/* XPM */ static char * arrow_left[] = { \"9 19 3 1\", \"0 c #f5f5f9\", \"1 c #f5f5f9\", \"2 c #f5f5f9\",\"111111111\",\"011111111\",\"001111111\",\"000111111\",\"000011111\",\"000001111\",\"000000111\",\"000000011\",\"000000001\",\"000000000\",\"000000001\",\"000000011\",\"000000111\",\"000001111\",\"000011111\",\"000111111\",\"001111111\",\"011111111\",\"111111111\",};" :ascent center :face powerline-active2)))) (mouse-color . "black") (environment) (visibility . t) (cursor-color . "#b294bb") (background-mode . light) (fullscreen) (alpha) (scroll-bar-height . 0) (scroll-bar-width . 0) (cursor-type . box) (auto-lower) (auto-raise) (icon-type) (title) (buffer-predicate) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 8) (left-fringe . 8) (line-spacing) (background-color . "#F5F5F9") (foreground-color . "#0F1019") (horizontal-scroll-bars) (vertical-scroll-bars) (internal-border-width . 2) (border-width . 0) (font . "-*-Monaco-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1") (fontsize . 0)) '(86 27 ((0 0 88 26)) (("~/sw/programming/go/src/github.com/fpdevil/goprog/random-stuff/general-stuff/word-frequencies/main.go" "main.go" 126 1 ((tab "*scratch*" "/Users/sampathsingamsetty/.emacs.d/preferences/scratch buffer") (tab "main.go" "/Users/sampathsingamsetty/sw/programming/go/src/github.com/fpdevil/goprog/random-stuff/general-stuff/word-frequencies/main.go"))))))))
+
+;; Global section:
+(setq desktop-saved-frameset [frameset 1 (24450 57082 549087 0) (desktop . "208") "sampathsingamsetty@apple" nil nil ((((font-backend mac-ct) (fontsize . 0) (font . "-*-Monaco-normal-normal-normal-*-14-*-*-*-m-0-iso10646-1") (border-width . 0) (internal-border-width . 2) (vertical-scroll-bars) (horizontal-scroll-bars) (foreground-color . "#0F1019") (background-color . "#F5F5F9") (line-spacing) (left-fringe . 8) (right-fringe . 8) (menu-bar-lines . 1) (tool-bar-lines . 1) (title) (icon-type) (auto-raise) (auto-lower) (cursor-type . box) (scroll-bar-width . 0) (scroll-bar-height . 0) (alpha) (fullscreen) (display-type . color) (background-mode . light) (cursor-color . "#b294bb") (visibility . t) (environment) (mouse-color . "black") (powerline-cache) (ns-transparent-titlebar . t) (ns-appearance . light) (frameset--id . "8301-633D-DAEB-3799") (frameset--mini t . t) (height . 27) (width . 86) (modeline . t) (minibuffer . t) (unsplittable) (left . 0) (top . 23) (right-divider-width . 0) (bottom-divider-width . 0) (icon-name) (display . "apple") (explicit-name) (tool-bar-position . top)) ((min-height . 4) (min-width . 10) (min-height-ignore . 4) (min-width-ignore . 6) (min-height-safe . 1) (min-width-safe . 2) (min-pixel-height . 76) (min-pixel-width . 80) (min-pixel-height-ignore . 58) (min-pixel-width-ignore . 48) (min-pixel-height-safe . 19) (min-pixel-width-safe . 16)) leaf (pixel-width . 705) (pixel-height . 502) (total-width . 88) (total-height . 26) (normal-height . 1.0) (normal-width . 1.0) (buffer "main.go" (selected . t) (hscroll . 0) (fringes 8 8 nil) (margins 2) (scroll-bars nil 0 t nil 0 nil) (vscroll . 0) (dedicated) (point . 126) (start . 1))))])
+(setq desktop-missing-file-warning nil)
+(setq tags-file-name nil)
+(setq tags-table-list nil)
+(setq search-ring nil)
+(setq regexp-search-ring nil)
+(setq register-alist nil)
+(setq file-name-history '("~/sw/programming/go/src/github.com/fpdevil/goprog/random-stuff/general-stuff/word-frequencies/main.go" "~/aquamacs"))
+
+;; Buffer section -- buffers listed in same order as in buffer list:
+(desktop-create-buffer 208
+  "/Users/sampathsingamsetty/sw/programming/go/src/github.com/fpdevil/goprog/random-stuff/general-stuff/word-frequencies/main.go"
+  "main.go"
+  'go-mode
+  '(visual-line-mode eldoc-mode global-auto-revert-mode gcmh-mode override-global-mode ycmd-mode flycheck-mode ycmd-eldoc-mode yas-minor-mode undo-tree-mode evil-commentary-mode beacon-mode flx-ido-mode git-gutter-mode rainbow-mode highlight-symbol-mode rainbow-delimiters-mode rainbow-identifiers-mode which-key-mode guide-key-mode company-mode auto-complete-mode rich-minority-mode smartparens-mode evil-smartparens-mode volatile-highlights-mode vi-tilde-fringe-mode highlight-quoted-mode highlight-numbers-mode)
+  126
+  '(126 nil)
+  nil
+  nil
+  '((show-trailing-whitespace . t) (indicate-empty-lines . t) (tab-width . 4) (indent-tabs-mode . t) (buffer-file-coding-system . undecided-unix) (overwrite-mode) (truncate-lines))
+  '((mark-ring nil)))
+
+(desktop-create-buffer 208
+  "/Users/sampathsingamsetty/.emacs.d/preferences/scratch buffer"
+  "*scratch*"
+  'fundamental-mode
+  '(visual-line-mode global-auto-revert-mode gcmh-mode override-global-mode yas-minor-mode undo-tree-mode evil-commentary-mode beacon-mode flx-ido-mode which-key-mode guide-key-mode company-mode rich-minority-mode smartparens-mode evil-smartparens-mode volatile-highlights-mode eldoc-overlay-mode drag-stuff-mode)
+  1
+  '(1 nil)
+  nil
+  nil
+  '((overwrite-mode) (truncate-lines))
+  '((mark-ring (1))))
+
+(desktop-create-buffer 208
+  "/Users/sampathsingamsetty/.emacs.d/customizations.el"
+  "customizations.el"
+  'fundamental-mode
+  '(global-auto-revert-mode gcmh-mode override-global-mode yas-minor-mode undo-tree-mode evil-commentary-mode beacon-mode flx-ido-mode which-key-mode guide-key-mode rich-minority-mode smartparens-mode evil-smartparens-mode volatile-highlights-mode)
+  1
+  '(nil nil)
+  nil
+  nil
+  '((buffer-file-coding-system . no-conversion) (overwrite-mode))
+  '((mark-ring nil)))
+
