5820b87b942107a39aab6855241027318987ca0f
fpdevil <singamsetty.sampath@gmail.com>
2020-10-11 12:27:25 -0500

    minor changes to smartparens

--- modules/sp-config.el
+++ modules/sp-config.el
@@ -37,34 +37,41 @@
  (sp-local-pair 'org-mode "~" "~" :actions '(wrap))
  (sp-local-pair 'org-mode "/" "/" :actions '(wrap))
  (sp-local-pair 'org-mode "*" "*" :actions '(wrap))
+ (sp-local-pair 'go-mode "{" nil :post-handlers '((custom-newline-and-enter-sexp "RET")))
+ (defun custom-newline-and-enter-sexp (&rest _ignored)
+   "Open a new brace or bracket expression, with relevant newlines and indent. "
+   (newline)
+   (indent-according-to-mode)
+   (forward-line -1)
+   (indent-according-to-mode))
 
  (sp-with-modes '(tex-mode
                   plain-tex-mode
                   latex-mode
                   org-mode)
-		(sp-local-pair "$" " $")
-		(sp-local-pair "\\[" " \\]")
-		(sp-local-pair "\\(" " \\)")
-		(sp-local-pair "\\{" " \\}")
-		(sp-local-pair "\\left(" " \\right)")
-		(sp-local-pair "\\left\\{" " \\right\\}"))
+   (sp-local-pair "$" " $")
+   (sp-local-pair "\\[" " \\]")
+   (sp-local-pair "\\(" " \\)")
+   (sp-local-pair "\\{" " \\}")
+   (sp-local-pair "\\left(" " \\right)")
+   (sp-local-pair "\\left\\{" " \\right\\}"))
 
  (sp-with-modes '(emacs-lisp-mode
                   inferior-emacs-lisp-mode
                   lisp-interaction-mode
                   lisp-mode)
-		(sp-local-pair "'" nil :actions nil)
-		(sp-local-pair "`" "'" :when '(sp-in-string-p) :actions '(insert wrap)))
+   (sp-local-pair "'" nil :actions nil)
+   (sp-local-pair "`" "'" :when '(sp-in-string-p) :actions '(insert wrap)))
 
  (sp-with-modes '(scala-mode)
-		(sp-local-pair "'" nil :actions nil))
+   (sp-local-pair "'" nil :actions nil))
 
  (sp-with-modes '(text-mode)
-		(sp-local-pair "`" "'" :actions '(insert wrap)))
+   (sp-local-pair "`" "'" :actions '(insert wrap)))
 
  (sp-with-modes '(markdown-mode
                   rst-mode)
-		(sp-local-pair "`" "`"))
+   (sp-local-pair "`" "`"))
 
  (setq sp-base-key-bindings 'paredit)
  (setq sp-autoskip-closing-pair 'always)
@@ -72,8 +79,8 @@
  (setq blink-matching-paren t)
  (setq sp-navigate-close-if-unbalanced t)
 
- (custom-set-faces
-  '(sp-pair-overlay-face ((t (:background "grey20")))))
+ ;; (custom-set-faces
+ ;;  '(sp-pair-overlay-face ((t (:background "grey20")))))
 
  ;; angle brackets aren’t treated as delimiters by default in js2-mode
  ;; below is the fix through sp
@@ -91,7 +98,7 @@
    (indent-according-to-mode)
    (forward-line -1)
    (indent-according-to-mode))
- 
+
  ;;** for c/c++ indentation
  (sp-local-pair 'c++-mode "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
 
@@ -104,7 +111,7 @@
 
  ;;** fix an error caused by smartparens interfering with yasnippet' bindings
  (after 'yasnippet
-	(advice-add #'yas-expand :before #'sp-remove-active-pair-overlay))
+   (advice-add #'yas-expand :before #'sp-remove-active-pair-overlay))
 
  )
 
